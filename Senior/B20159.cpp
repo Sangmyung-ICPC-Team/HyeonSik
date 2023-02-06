ccf81c6530a0b24c12fd53ba40a601a8fa7c2252
HyeonSik <100890581+HYEONSIKOH@users.noreply.github.com>
2023-02-06 15:21:24 +0900

    0207 - BaekJoon 20159 [누적합] 동작 그만. 밑장 빼기냐?

--- Senior/B20159.cpp
+++ Senior/B20159.cpp
@@ -3,17 +3,17 @@
 using namespace std;
 
 int main() {
-	  ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL);
+	ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL);
 
-	  int n;
-	  cin >> n;
+	int n;
+	cin >> n;
 
     vector<int> vec(n + 1, 0);
 
     vector<int> even_bsum(n/2 + 1, 0); // 짝수 bsum
     vector<int> odd_psum(n/2 + 1, 0);  // 홀수 psum
 
-    for (int i = 1; i <= n; i++) cin >> vec[i];
+    for (int i = 1; i<= n; i++) cin >> vec[i];
 	
     int b_idx = n;
     int p_idx = 1;
@@ -26,15 +26,15 @@ int main() {
     }
 
     int ans = 0;
-	  for (int i = 1; i <= n/2; i++) {
+	for (int i = 1; i <= n/2; i++) {
         int a, b;
 
-		    a = odd_psum[i-1] + even_bsum[n/2 - (i - 1)];
+		a = odd_psum[i-1] + even_bsum[n/2 - (i - 1)];
         b = odd_psum[i] + even_bsum[n/2 - (i - 1)] - even_bsum[1];
 
         ans = max(ans, max(a, b));
-	  }
+	}
 
-	  cout << ans;
-	  return 0;
+	cout << ans;
+	return 0;
 }
